b21895acbe8d8a6dcdebf0d92be535b3faec0833
Ryan <jaewoopk000@naver.com>
2022-09-18 13:02:55 +0900

    Day 4 Solve Exercise 2-5

---
+++ moveBlock.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include <windows.h>
+
+char blockModel[][4][4] =
+{
+    {
+        {0, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0} },
+ 
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0} },   
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0} },   
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0} },
+ 
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 0},
+        {0, 1, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0} },
+ 
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0} },  
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0} },   
+    {
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0} },
+ 
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0} },  
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0} },   
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0} },   
+    {
+        {1, 1, 0, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0} },
+ 
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0} },   
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {1, 1, 0, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0} }, 
+ 
+    {
+        {0, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0} },
+    {  
+        {0, 0, 0, 0},
+        {1, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0} },  
+    {
+        {0, 1, 0, 0},
+        {1, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 0} },
+};
+
+void DeleteBlock(char blockInfo[4][4]);
+void ShowBlock(char blockInfo[4][4]);
+void SetCurrentCursorPos(int x, int y);
+COORD GetCurrentCursorPos(void);
+
+int main(void) {
+    for (int i = 0; i < 5; i++) {
+        SetCurrentCursorPos(i * 2, i);
+        ShowBlock(blockModel[0]);
+        Sleep(500);
+        DeleteBlock(blockModel[0]);
+    }
+    ShowBlock(blockModel[0]);
+    getchar();
+    return (0);
+}
+
+void DeleteBlock(char blockInfo[4][4]) {
+    int y, x;
+    COORD curPos = GetCurrentCursorPos();
+
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 4; x++) {
+            SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
+
+            if (blockInfo[y][x] == 1) {
+                printf("  ");
+            }
+        }
+    }
+    SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+void ShowBlock(char blockInfo[4][4]) {
+    int x, y;
+    COORD curPos = GetCurrentCursorPos();
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 4; x++) {
+            SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
+
+            if (blockInfo[y][x] == 1) {
+                printf("бс");
+            }
+        }
+    }
+    SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+void SetCurrentCursorPos(int x, int y) {
+    COORD pos = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+COORD GetCurrentCursorPos(void) {
+    COORD curPoint;
+    CONSOLE_SCREEN_BUFFER_INFO curInfo;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+    curPoint.X = curInfo.dwCursorPosition.X;
+    curPoint.Y = curInfo.dwCursorPosition.Y;
+
+    return curPoint;
+}
